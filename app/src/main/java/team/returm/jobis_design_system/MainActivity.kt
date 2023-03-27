379e07e4948d665150b9b004e2d3bbd06a639143
Tmdhoon2 <hks026naver@gmail.com>
2023-03-27 09:14:21 +0900

    🧩 :: implement MainActivity

---
+++ app/src/main/java/team/returm/jobis_design_system/MainActivity.kt
@@ -0,0 +1,14 @@
+package team.returm.jobis_design_system
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity: ComponentActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent{
+
+        }
+    }
+}
