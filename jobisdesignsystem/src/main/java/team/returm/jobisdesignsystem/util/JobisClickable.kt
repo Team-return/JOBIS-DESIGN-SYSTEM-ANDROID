ec1365f544e8865541f41132f18c8a692a54bbcd
Tmdhoon2 <hks026naver@gmail.com>
2023-07-10 07:29:51 +0900

    🐛 :: Button interaction source 적용

--- jobisdesignsystem/src/main/java/team/returm/jobisdesignsystem/util/JobisClickable.kt
+++ jobisdesignsystem/src/main/java/team/returm/jobisdesignsystem/util/JobisClickable.kt
@@ -3,6 +3,7 @@ package team.returm.jobisdesignsystem.util
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -10,15 +11,17 @@ import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 
 @Stable
+@Composable
 fun Modifier.jobisClickable(
     rippleEnabled: Boolean = false,
     enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
     onClick: () -> Unit,
 ) = composed {
     Modifier.clickable(
         onClick = onClick,
         enabled = enabled,
-        interactionSource = remember { MutableInteractionSource() },
+        interactionSource = interactionSource,
         indication = rememberRipple(
             color = Color.Unspecified,
         ).takeIf {
